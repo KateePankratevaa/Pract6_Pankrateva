eae4991fa28aec1607b23c2a2420a4a27b12206c
KateePankratevaa <124819002+KateePankratevaa@users.noreply.github.com>
2024-02-10 11:04:34 +0500

    Add files via upload

---
+++ Pract6_Pankrateva/Pract6_Pankrateva.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+
+void delete_gl(char s[50], char str[])
+{
+    int j = 0;
+    for (int i = 0; s[i]; i++)
+        if (!strchr(str, s[i]))
+
+            s[j++] = s[i];
+            s[j] = 0;
+}
+
+    int main(void)
+    {
+        setlocale(LC_ALL, "Russian");
+
+        char s[50];
+        char str[] = "AaEeIiOoUu";
+        int n;
+        printf("Введите строку на английском: ");
+        gets_s(s);
+        n = strlen(s);
+        delete_gl(s, str);
+        
+        puts(s);
+    }
+
